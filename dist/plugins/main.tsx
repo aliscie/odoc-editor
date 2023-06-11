c75c4f10fc5ecb691b0617c876555870d9f521a0
aliscie <alihushamsci@icloud.com>
2023-06-11 18:03:04 +0300

    build command

--- dist/plugins/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {HoveringToolbar} from "./toolbar";
-
-let plugins = (editor?: any) => {
-    return [
-        <HoveringToolbar/>
-    ]
-}
-export default plugins;
--- dist/utiles/generate_id.tsx
+++
@@ -1,3 +0,0 @@
-export function generateId(): string {
-    return "id_" + Math.random().toString(36).substring(2, 15)
-}
